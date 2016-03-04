3b967542c2b5debdad38bb989d49cb389330ba48
justbo <b.tsybulskyy@gmail.com>
2016-03-04 00:39:52 +0000

    commirs

--- main.js
+++ main.js
@@ -1,3 +1,27 @@
 $(function(){
-    
+	$('.controller').click(function() {
+  	var item  = $('.slider .item');
+  	var nmbSl = item.length;
+    var i = -1;
+    var curAct = 0;
+    //curent active item
+    $('.slider .item').each(function() {
+      ++i;
+      if($(this).hasClass('active')){
+      	  curAct = i;
+				};
+      });
+    //next active item
+    var nextAct = curAct;
+    if($(this).hasClass('left')){
+    	var nextAct = (curAct-1) % nmbSl;
+      if(nextAct<0){
+      	nextAct = nmbSl-1;
+      }
+    }else if ($(this).hasClass('right')){
+    	var nextAct = (curAct+1) % nmbSl;
+    };
+    $(item[curAct]).removeClass('active');
+    $(item[nextAct]).addClass('active');
+  });
 });
