809681eb9a3c0f0dfb6204866b7d34c015d30098
justbo <b.tsybulskyy@gmail.com>
2016-03-05 22:40:15 +0000

    end

--- main.js
+++ main.js
@@ -1,27 +1,44 @@
-$(function(){
-	$('.controller').click(function() {
-  	var item  = $('.slider .item');
-  	var nmbSl = item.length;
-    var i = -1;
-    var curAct = 0;
-    //curent active item
-    $('.slider .item').each(function() {
-      ++i;
-      if($(this).hasClass('active')){
-      	  curAct = i;
-				};
-      });
-    //next active item
-    var nextAct = curAct;
+ var item  = $('.slider .item');
+  var nmbSl = item.length;
+  var curAct = curPosition();
+  var circle = $('.contr-circle li');
+  var nextAct = curAct+1;
+  setInterval(function(){
+    curAct  = curPosition();
+    nextAct = (curAct+1) % nmbSl;
+    change();
+  },4000);
+  $('.contr-circle li').click(function() {
+    curAct  = curPosition();
+    nextAct = $(this).data('slide');
+    change();
+  });
+  $('.controller').click(function() {
+    curAct = curPosition();
     if($(this).hasClass('left')){
-    	var nextAct = (curAct-1) % nmbSl;
+    	nextAct = (curAct-1) % nmbSl;
       if(nextAct<0){
       	nextAct = nmbSl-1;
       }
     }else if ($(this).hasClass('right')){
-    	var nextAct = (curAct+1) % nmbSl;
+    	nextAct = (curAct+1) % nmbSl;
     };
-    $(item[curAct]).removeClass('active');
-    $(item[nextAct]).addClass('active');
+    change();
   });
-});
+  function curPosition() {
+    var res;
+    var i = -1;
+    $('.slider .item').each(function() {
+       ++i;
+       if($(this).hasClass('active')){
+        	  return res = i;
+  				}
+        });
+    return res;   
+  };
+  function change() {
+    $(item[curAct]).removeClass('active').animate({opacity:0});
+    $(item[nextAct]).animate({opacity:1},1000).addClass('active');
+    $(circle[curAct]).removeClass('circ-act');
+    $(circle[nextAct]).addClass('circ-act');
+  };
